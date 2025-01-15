afddcf5f5087e15ea89a931c44201ad85e32e265
Eugeny Burnashov <shamcode@gmail.com>
2025-01-15 12:55:21 +0700

    Add tests

--- __tests__/unit/column-table.js
+++ __tests__/unit/column-table.js
@@ -1,7 +1,21 @@
 import renderer from 'sham-ui-test-helpers';
-import ColumnTable from '../../src/column-table.sfc';
+import ColumnTable, { resolver } from '../../src/column-table.sfc';
 
 it( 'renders correctly', () => {
     const meta = renderer( ColumnTable, {} );
+    expect( meta.toJSON() ).toMatchSnapshot();
+} );
+
+it( 'resolver', () => {
+    const meta = renderer( ColumnTable, {
+        columns: [
+            { title: 'Foo', data: 'foo' }
+        ],
+        items: [ 1, 2, 3 ],
+        resolver: column => ( {
+            ...resolver( column ),
+            titleGetter: () => 'Bar'
+        } )
+    } );
     expect( meta.toJSON() ).toMatchSnapshot();
 } );
