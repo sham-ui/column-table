ac50a6f924009eca0288021ab0c906a898349859
Eugeny Burnashov <shamcode@gmail.com>
2025-01-21 12:54:17 +0700

    Add tests

--- __tests__/unit/resolver.js
+++ __tests__/unit/resolver.js
@@ -1,7 +1,16 @@
-import resolver from '../../src/resolver';
+import resolver, { resolveTitle } from '../../src/resolver';
 
 it( 'renders correctly', () => {
     const data = resolver( {
+        title: 'Foo',
+        data: 'Bar'
+    } );
+    expect( data ).toMatchSnapshot();
+} );
+
+
+it( 'resolveTitle', () => {
+    const data = resolveTitle( {
         title: 'Foo',
         data: 'Bar'
     } );
